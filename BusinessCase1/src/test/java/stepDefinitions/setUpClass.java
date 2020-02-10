53e11209727ad4fab6a1518181d30d7c4ab2b585
cesar-perez-bernal <60890278+cesar-perez-bernal@users.noreply.github.com>
2020-02-10 12:32:38 -0600

    back

--- BusinessCase1/src/test/java/stepDefinitions/setUpClass.java
+++ BusinessCase1/src/test/java/stepDefinitions/setUpClass.java
@@ -1,10 +1,8 @@
 package stepDefinitions;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -15,17 +13,7 @@ public class setUpClass {
 	@Before
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "src//test//resources//Drivers//chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-features=VizDisplayCompositor");
-		 options.addArguments("start-maximized");
-	        options.addArguments("enable-automation"); 
-	        options.addArguments("--headless"); 
-	        options.addArguments("--no-sandbox"); 
-	        options.addArguments("--disable-infobars"); 
-	        options.addArguments("--disable-dev-shm-usage"); 
-	        options.addArguments("--disable-browser-side-navigation"); 
-	        options.addArguments("--disable-gpu"); 
-		driver = new ChromeDriver(options);
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://www.automationpractice.com/index.php");
