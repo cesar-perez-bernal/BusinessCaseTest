df7c73d11a396d849a8933fa53e8c1f9400b295a
cesar-perez-bernal <60890278+cesar-perez-bernal@users.noreply.github.com>
2020-02-10 11:25:53 -0600

    change tag

--- BusinessCase1/src/test/java/testRunners/loginRunner.java
+++ BusinessCase1/src/test/java/testRunners/loginRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 			features="src/test/resources/Features",
 			glue= {"stepDefinitions"},
-			tags= {"not @ignore"}
+			tags= {"@tag"}
 			
 			)
 	
