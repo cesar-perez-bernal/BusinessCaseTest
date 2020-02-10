0d4c94caecc6bc987e64e2122a7c0e4ea924b426
cesar-perez-bernal <60890278+cesar-perez-bernal@users.noreply.github.com>
2020-02-10 12:27:00 -0600

    more options

--- BusinessCase1/src/test/java/stepDefinitions/setUpClass.java
+++ BusinessCase1/src/test/java/stepDefinitions/setUpClass.java
@@ -17,6 +17,14 @@ public class setUpClass {
 		System.setProperty("webdriver.chrome.driver", "src//test//resources//Drivers//chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-features=VizDisplayCompositor");
+		 options.addArguments("start-maximized");
+	        options.addArguments("enable-automation"); 
+	        options.addArguments("--headless"); 
+	        options.addArguments("--no-sandbox"); 
+	        options.addArguments("--disable-infobars"); 
+	        options.addArguments("--disable-dev-shm-usage"); 
+	        options.addArguments("--disable-browser-side-navigation"); 
+	        options.addArguments("--disable-gpu"); 
 		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
